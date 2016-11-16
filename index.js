ed2218a1b1eb9bdcf3cbd87fd4e665cc5342f030
Jake Nielsen <jake.k.nielsen@gmail.com>
2016-11-15 17:55:01 -0800

    First Commit

---
+++ index.js
@@ -0,0 +1,202 @@
+var _           = require('lodash');
+var async       = require('async');
+var pg          = require('pg');
+
+function PostgresDB (opts) {
+  var self = this;
+  if (!opts.host)                   throw new Error('PostgresDB must be initialized with a host in the options argument');
+  if  (opts.user === undefined)     throw new Error('PostgresDB must be initialized with a user in the options argument');
+  if  (opts.password === undefined) throw new Error('PostgresDB must be initialized with a password in the options argument');
+  if (!opts.database)               throw new Error('PostgresDB must be initialized with a database in the options argument');
+  if (!opts.port)                   throw new Error('PostgresDB must be initialized with a port in the options argument');
+  
+  self._pgConnectionConfig = {
+    host: opts.host,
+    user: opts.user,
+    password: opts.password,
+    database: opts.database,
+    port: opts.port,
+    max: 4,
+    idleTimeoutMillis: 30000,
+  };
+
+  self._typeMapping = {};
+  self._initialization_callbacks = [];
+  self._initialize(5);
+}
+
+var CreateDB = function (config, cb) {
+  var database = config.database;
+  var pgUrl = `postgres://${config.user}:${config.password}@${config.host}:${config.port}/template1`;
+  var db = new pg.Client(pgUrl);
+
+  db.connect(function (err) {
+    if (err) return cb(err);
+    db.query(`CREATE DATABASE "${database}"`, function (err) {
+      db.end();
+      cb();
+    })
+  })
+}
+
+PostgresDB.prototype._initialize = function (retries) {
+  var self = this;
+
+  var createDatabase = function (cb) {
+    CreateDB(self._pgConnectionConfig, cb);
+  }
+
+  createDatabase(function (err) {
+    //ignore the error and just keep going
+    self._db = new pg.Pool(self._pgConnectionConfig);
+    var db = self._db;
+
+    db.query(`CREATE TABLE IF NOT EXISTS NoSQLPostgresTypeMapping (tableName TEXT, columnName TEXT, type TEXT);`, function (err) {
+      if (err) throw new Error(`Error while initializing PostgresDB ${err}`);
+      db.query(`SELECT * FROM NoSQLPostgresTypeMapping`, function (err, results) {
+        if (err) throw new Error(`Error while initializing PostgresDB ${err}`);
+        results.rows.forEach(function (row) {
+          _.set(self._typeMapping, [row.tableName, row.columnName], row.type);
+        })
+
+        self._initialized = true;
+        self._initialization_callbacks.forEach(function(fn) {
+          fn();
+        });
+        delete self._initialization_callbacks;
+      })
+    });
+  });
+}
+
+PostgresDB.prototype._prepTable = function (tableName, cb) {
+  var self = this;
+  if (self._typeMapping[tableName]) return cb();
+
+  self._db.query(`CREATE TABLE IF NOT EXISTS ${tableName} ("postgresId" TEXT);
+    CREATE UNIQUE INDEX IF NOT EXISTS ${tableName}Index ON ${tableName} ("postgresId");`, function (err) {
+    if (err) return cb(err);
+    self._db.query(`INSERT INTO NoSQLPostgresTypeMapping (tableName, columnName, type) VALUES ($1, $2, $3)`, [tableName, 'postgresId', 'string'], function (err) {
+      if (err) return cb(err);
+      self._typeMapping[tableName] = {postgresId: 'string'};
+      cb();
+    });
+  });
+}
+
+var determineStorageType = function (object) {
+  if (object instanceof Date) return 'date';
+  if (_.isString(object)) return 'string';
+  if (_.isNumber(object)) return 'number';
+  return 'object';
+}
+
+var renderType = function (object, type) {
+  switch (type) {
+    case 'date': return object.toISOString();
+    case 'string': return object;
+    case 'number': return JSON.stringify(object);
+    case 'object': return JSON.stringify(object);
+    default: throw new Error(`Unknown type ${type}`)
+  }
+}
+
+var reconstructType = function (object, type) {
+  switch (type) {
+    case 'date': return new Date(object);
+    case 'string': return object;
+    case 'number': return JSON.parse(object);
+    case 'object': return JSON.parse(object);
+    default: throw new Error(`Unknown type ${type}`)
+  }
+}
+
+var typeMap = {
+  'date': 'TEXT',
+  'string': 'TEXT',
+  'number': 'TEXT',
+  'object': 'TEXT',
+}
+
+PostgresDB.prototype._prepColumn = function (tableName, colName, type, cb) {
+  var self = this;
+  self._db.query(`ALTER TABLE ${tableName} ADD COLUMN "${colName}" ${typeMap[type]}`, function (err) {
+    self._db.query(`INSERT INTO NoSQLPostgresTypeMapping (tableName, columnName, type) VALUES ($1, $2, $3)`, [tableName, colName, type], function (err) {
+      if (err) return cb(err);
+      self._typeMapping[tableName][colName] = type;
+      cb();
+    });
+  });
+}
+
+PostgresDB.prototype._prepareForTableAndObject = function (tableName, object, cb) {
+  var self = this;
+  self._prepTable(tableName, function (err) {
+    if (err) return cb(err);
+    async.each(Object.keys(object), function (key, asyncCB) {
+      if (self._typeMapping[tableName][key]) return asyncCB();
+      self._prepColumn(tableName, key, determineStorageType(object[key]), asyncCB);
+    }, cb);
+  });
+}
+
+PostgresDB.prototype._afterInitialization = function (cb) {
+  if (this._initialized) return setImmediate(cb);
+  this._initialization_callbacks.push(cb);
+}
+
+
+
+PostgresDB.prototype.upsertObject = function (tableName, id, object, cb) {
+  var self = this;
+  self._afterInitialization(function () {
+    self._prepareForTableAndObject(tableName, object, function (err) {
+      if (err) return cb(err);
+      var keys = Object.keys(object);
+      var d = 0;
+      var dollar = function () {
+        d++;
+        return '$' + d;
+      }
+      var query = `INSERT INTO ${tableName} ("postgresId", ${keys.map(k => '"' + k + '"').join(',')}) VALUES (${dollar()}, ${keys.map(k => dollar()).join(',')})
+      ON CONFLICT ("postgresId") DO UPDATE SET ${keys.map(k => '"' + k + '"' + ' = ' + dollar()).join(',')};`;
+      var renderedValues = keys.map(k => renderType(object[k], self._typeMapping[tableName][k]));
+      var queryValues = [id];
+      renderedValues.forEach(function (value) {
+        queryValues.push(value);
+      })
+      renderedValues.forEach(function (value) {
+        queryValues.push(value);
+      })
+      self._db.query(query, queryValues, cb);
+    });
+  });
+}
+
+PostgresDB.prototype.getObject = function (tableName, id, cb) {
+  var self = this;
+  self._afterInitialization(function () {
+    self._db.query(`SELECT * FROM ${tableName} WHERE "postgresId" = $1`, [id], function (err, results) {
+      if (err) return cb(err);
+      var result = results.rows[0];
+      if (!result) return cb(null, null);
+      var finalResult = {};
+      Object.keys(result).forEach(function (key) {
+        if (key === 'postgresId') return;
+        finalResult[key] = reconstructType(result[key], self._typeMapping[tableName][key]);
+      });
+      return cb(null, finalResult);
+    });
+  });
+}
+
+
+
+
+module.exports = PostgresDB;
+
+
+
+
+
+
---
+++ test/simple.js
@@ -0,0 +1,107 @@
+var assert  = require('assert');
+var uuid    = require('node-uuid')
+var pg      = require('pg');
+var PostgresDB = require('../');
+
+var testConfiguration = {
+  host:'localhost',
+  user: '',
+  password: '',
+  database: 'pgnosqltest',
+  port:5432,
+  lockExpirationTime: 60
+};
+
+describe('Simple Tests', function () {
+  before(function (done) {
+    var db = new pg.Client('postgres://localhost/template1');
+    db.connect(function (err) {
+      if (err) return done();
+      db.query(`DROP DATABASE "pgnosqltest"`, function (err) {
+        db.end(function (err) {
+          done();
+        });
+      });
+    });
+  })
+
+  it('Create a db object', function (done) {
+    var db = new PostgresDB(testConfiguration);
+    db._afterInitialization(done);
+  });
+
+  it('Insert an object', function (done) {
+    var db = new PostgresDB(testConfiguration);
+    var objectId = uuid.v4();
+    db.upsertObject('testTable', objectId, {key: 'value'}, function (err) {
+      assert.equal(err, null);
+      done();
+    });
+  });
+
+  it('Insert and retrieve object', function (done) {
+    var db = new PostgresDB(testConfiguration);
+    var objectId = uuid.v4();
+    db.upsertObject('testTable2', objectId, {key: 'value'}, function (err) {
+      assert.equal(err, null);
+      db.getObject('testTable2', objectId, function (err, result) {
+        assert.equal(err, null);
+        assert.equal(result.key, 'value');
+        done();
+      })
+    });
+  });
+
+  it('Insert and retrieve object with capitalized keys', function (done) {
+    var db = new PostgresDB(testConfiguration);
+    var objectId = uuid.v4();
+    db.upsertObject('testTable3', objectId, {Key: 'value'}, function (err) {
+      assert.equal(err, null);
+      db.getObject('testTable3', objectId, function (err, result) {
+        assert.equal(err, null);
+        assert.equal(result.Key, 'value');
+        done();
+      })
+    });
+  });
+
+  it('Upsert several into the same object', function (done) {
+    var db = new PostgresDB(testConfiguration);
+    var objectId = uuid.v4();
+    db.upsertObject('testTable4', objectId, {Key: 'value'}, function (err) {
+      assert.equal(err, null);
+      db.upsertObject('testTable4', objectId, {key: 'value'}, function (err) {
+        assert.equal(err, null);
+        db.getObject('testTable4', objectId, function (err, result) {
+          assert.equal(err, null);
+          assert.equal(result.Key, 'value');
+          assert.equal(result.key, 'value');
+          done();
+        });
+      });
+    });
+  });
+
+  it('Upsert several types', function (done) {
+    var db = new PostgresDB(testConfiguration);
+    var objectId = uuid.v4();
+
+    var ob = {
+      Key: {
+        somethingDeep: 'bloop',
+      },
+      num: 12,
+      othernum: '12',
+      alist: [1, 2, 3, '4']
+    }
+
+    db.upsertObject('testTable5', objectId, ob, function (err) {
+      assert.equal(err, null);
+      db.getObject('testTable5', objectId, function (err, result) {
+        assert.equal(err, null);
+        assert.deepEqual(result, ob);
+        done();
+      });
+    });
+  });
+});
